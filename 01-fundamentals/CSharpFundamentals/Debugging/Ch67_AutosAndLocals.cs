5d2f08ebc4db7c772bb90479176139c479162fd7
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-06 15:47:34 +0200

    Ch67 Local and Autos

--- 01-fundamentals/CSharpFundamentals/Debugging/Ch67_AutosAndLocals.cs
+++ 01-fundamentals/CSharpFundamentals/Debugging/Ch67_AutosAndLocals.cs
@@ -17,9 +17,43 @@ namespace CSharpFundamentals.Debugging
     {
         public static void Run()
         {
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var smallests = GetSmallests(numbers, 3);
+
+            foreach (var number in smallests)
+                Console.WriteLine(number);
+
+            Console.ReadLine();
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            if (count > list.Count || count <= 0)
+                throw new ArgumentOutOfRangeException("count", "Count should be between 1 and the number of elements in this list."); // Meaningful Exception Message
 
 
-            Console.ReadKey();
+            var buffer = new List<int>(list);
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var minim = GetSmallest(buffer);
+                smallests.Add(minim);
+                buffer.Remove(minim);
+            }
+            return smallests;
+        }
+        public static int GetSmallest(List<int> list)
+        {
+            //Assume the first number is the smallest
+            var min = list[0]; 
+
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min) //autos window selects automatically variables that vs deems vital, at the current location the user is at the program
+                    min = list[i]; //locals isn't as resourceful and displays less variety of variables. Choose locals or autos based on reference
+            }
+            return min;
         }
     }
 }
