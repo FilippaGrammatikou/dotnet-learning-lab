ecb9cf2b44a4501f8d2b77183410900817b68365
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-08 17:56:11 +0200

    Install NuGet Packages and Dependencies

---
+++ 03-portfolio-project/BookReviewApp/DATA/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookReviewApp.DATA
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions) { }
+    }
+}
