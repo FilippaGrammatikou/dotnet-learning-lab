11b3fe5be1f6d9ee7ebf9854f99fa9072215daf3
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-09 18:43:29 +0200

    API Endpoint Creation

---
+++ 03-portfolio-project/BookReviewApp/Controllers/BooksController.cs
@@ -0,0 +1,30 @@
+﻿using BookReviewApp.Interfaces;
+using BookReviewApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        // API Endpoint
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Book>))]
+        public IActionResult GetBooks()
+        {
+            var books = _bookRepository.GetAllBooks();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(books);
+        }
+    }
+}
