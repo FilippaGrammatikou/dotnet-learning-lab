2a332cc9fcbdeae49df1a3a2bc6c4859f789fa01
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-10 21:13:19 +0200

    Category Repository Set Up

---
+++ 03-portfolio-project/BookReviewApp/Repository/CategoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using BookReviewApp.DATA;
+using BookReviewApp.Models;
+using BookReviewApp.Interfaces;
+
+namespace BookReviewApp.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly DataContext _context;
+        public CategoryRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public ICollection<Category> GetCategories()
+        {
+            return _context.Categories.OrderBy(c=>c.Id).ToList();
+        }
+        public Category GetCategory(int categoryId)
+        {
+            return _context.Categories.FirstOrDefault(c => c.Id == categoryId);
+        }
+        public ICollection<Book> GetBooksByCategory(int categoryId)
+        {
+            return _context.BookCategories.Where(bc=>bc.CategoryId == categoryId)
+                .Select(bc=>bc.Book)
+                .ToList();
+        }
+        public bool CategoryExists(int categoryId)
+        {
+            return _context.Categories.Any(c=>c.Id== categoryId);
+        }
+    }
+}
