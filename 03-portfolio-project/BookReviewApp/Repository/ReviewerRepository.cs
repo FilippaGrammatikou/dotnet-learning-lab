976ee0e2ca0ea90ff6b234b0df91f5026a207f21
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-13 15:22:39 +0200

    ReviewerRepository SetUp

---
+++ 03-portfolio-project/BookReviewApp/Repository/ReviewerRepository.cs
@@ -0,0 +1,34 @@
+﻿using BookReviewApp.DATA;
+using BookReviewApp.Interfaces;
+using BookReviewApp.Models;
+
+namespace BookReviewApp.Repository
+{
+    public class ReviewerRepository : IReviewerRepository
+    {
+        private readonly DataContext _context;
+        public ReviewerRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public Reviewer GetReviewer(int reviewerId)
+        {
+            return _context.Reviewers.FirstOrDefault(r => r.Id == reviewerId);
+        }
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            return _context.Reviewers.ToList();
+        }
+
+        public ICollection<Review> GetReviewsByReviewer(int reviewerId)
+        {
+            return _context.Reviews.Where(r => r.Reviewer.Id == reviewerId).ToList();
+        }
+
+        public bool ReviewerExists(int reviewerId)
+        {
+            return _context.Reviewers.Any(r => r.Id == reviewerId);
+        }
+    }
+}
