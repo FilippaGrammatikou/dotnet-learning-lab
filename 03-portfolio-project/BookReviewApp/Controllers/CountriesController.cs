0f4a862b4fa348464088af8f0f73bcf62c2f0aa8
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-13 14:18:55 +0200

    All Endpoints for CountriesController SetUp

--- 03-portfolio-project/BookReviewApp/Controllers/CountriesController.cs
+++ 03-portfolio-project/BookReviewApp/Controllers/CountriesController.cs
@@ -1,9 +1,12 @@
-﻿using BookReviewApp.Interfaces;
+﻿using BookReviewApp.DTO;
+using BookReviewApp.Interfaces;
 using BookReviewApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookReviewApp.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class CountriesController : ControllerBase
     {
         readonly ICountryRepository _countryRepository;
@@ -13,40 +16,89 @@ namespace BookReviewApp.Controllers
         }
 
         [HttpGet]
-        [ProducesResponseType(200, Type = typeof(IEnumerable<Country>))]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<CountryDto>))]
         public IActionResult GetCountries()
         {
             var countries = _countryRepository.GetCountries();
+            var countryDtos = countries.Select(c => new CountryDto
+            {
+                Id = c.Id,
+                Name = c.Name
+            }).ToList();
+
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
             return Ok(countries);
         }
 
         [HttpGet("{countryId}")]
-        [ProducesResponseType(200, Type = typeof(Country))]
+        [ProducesResponseType(200, Type = typeof(CountryDto))]
         [ProducesResponseType(400)]
         public IActionResult GetCountry(int countryId)
         {
             if (!_countryRepository.CountryExists(countryId))
-                return BadRequest("Country not Found");
+                return BadRequest("Country not found.");
+
             var country = _countryRepository.GetCountry(countryId);
+            var countryDto = new CountryDto
+            {
+                Id = country.Id,
+                Name = country.Name
+            };
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
             return Ok(country);
         }
 
         [HttpGet("author/{authorId}")]
-        [ProducesResponseType(200, Type = typeof(Country))]
+        [ProducesResponseType(200, Type = typeof(CountryDto))]
         [ProducesResponseType(400)]
         public IActionResult GetCountryByAuthor(int authorId)
         {
-            if (!_countryRepository.CountryExists(authorId))
-                return BadRequest("Country for the author Not Found");
             var country = _countryRepository.GetCountryByAuthor(authorId);
+            var countryDto = new CountryDto
+            {
+                Id = country.Id,
+                Name = country.Name
+            };
+            if (country == null)
+                return NotFound("Author not found or has no country assigned");
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
             return Ok(country);
         }
+
+        [HttpGet("{countryId}/authors")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Author>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetAuthorsFromCountry(int countryId)
+        {
+            if (!_countryRepository.CountryExists(countryId))
+                return BadRequest("Country not found.");
+            var authors = _countryRepository.GetAuthorsFromCountry(countryId);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(authors);
+        }
+
+        [HttpPut("{countryId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult UpdateCountry(int countryId, [FromBody] Country updatedCountry)
+        {
+            if (updatedCountry == null)
+                return BadRequest("Country data is null.");
+            if (countryId != updatedCountry.Id)
+                return BadRequest("Country ID mismatch.");
+            if (!_countryRepository.CountryExists(countryId))
+                return BadRequest("Country not found.");
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var success = _countryRepository.UpdateCountry(updatedCountry);
+            if (!success)
+                return StatusCode(500, "An error occurred while updating the country.");
+            return NoContent();
+        }
+
     }
 }
-    
