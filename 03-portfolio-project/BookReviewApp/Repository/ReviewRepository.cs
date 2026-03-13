a0c58f3f68389bba546840da5d1b73fee5f75f1c
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-13 15:11:18 +0200

    ReviewRepository Implementation and SetUp

---
+++ 03-portfolio-project/BookReviewApp/Repository/ReviewRepository.cs
@@ -0,0 +1,36 @@
+﻿using BookReviewApp.DATA;
+using BookReviewApp.Interfaces;
+
+namespace BookReviewApp.Repository
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        private readonly DataContext _context;
+        public ReviewRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Models.Review> GetReviews()
+        {
+            return _context.Reviews.ToList();
+        }
+
+        public Models.Review GetReview(int reviewId)
+        {
+            return _context.Reviews.FirstOrDefault(r => r.Id == reviewId);
+        }
+
+        public ICollection<Models.Review> GetReviewsOfBook(int bookId)
+        {
+            return _context.Reviews
+                .Where(r => r.Book.Id == bookId)
+                .ToList();
+        }
+
+        public bool ReviewExists(int reviewId)
+        {
+            return _context.Reviews.Any(r => r.Id == reviewId);
+        }
+    }
+}
