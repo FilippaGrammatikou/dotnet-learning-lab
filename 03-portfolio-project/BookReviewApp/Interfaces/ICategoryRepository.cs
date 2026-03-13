9cc3fc1d42a7468d4070c7432526b4802a0713e7
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-13 15:31:03 +0200

    Update,Create,Delete & Save Category

--- 03-portfolio-project/BookReviewApp/Interfaces/ICategoryRepository.cs
+++ 03-portfolio-project/BookReviewApp/Interfaces/ICategoryRepository.cs
@@ -6,6 +6,10 @@ namespace BookReviewApp.Interfaces
         ICollection<Category> GetCategories();
         Category GetCategory(int categoryId);
         ICollection<Book> GetBooksByCategory(int categoryId);
+        bool UpdateCategory(Category category);
         bool CategoryExists(int categoryId);
+        bool CreateCategory(Category category);
+        bool DeleteCategory(Category category);
+        bool Save();
     }
 }
